3cb57c005b9fab991e781708af3ed5576c6b4d7c
Lekhov Ilya <solwanlomtohs@mail.ru>
2022-02-20 11:17:41 +0300

    Final project

---
+++ Netology_IB_Instrument/ProfileView.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instrument
+//
+//  Created by Илья Лехов on 18.02.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet var mainImage: UIImageView!
+    
+    @IBOutlet var nameLabel: UILabel!
+    
+    @IBOutlet var dateLabel: UILabel!
+    
+    @IBOutlet var cityLabel: UILabel!
+    
+    @IBOutlet var descriptionText: UITextView!
+    
+    
+}
--- Netology_IB_Instrument/ViewController.swift
+++ Netology_IB_Instrument/ViewController.swift
@@ -12,6 +12,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if let subview = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? UIView {
+                    self.view.addSubview(subview)
+                    subview.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
+                }
+
     }
 
 

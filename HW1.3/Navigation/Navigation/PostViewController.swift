4b496e5eb036fb35d8624cbe2351ba4494aeac38
Lekhov Ilya <solwanlomtohs@mail.ru>
2022-02-20 20:18:33 +0300

    Created working tab bar

--- HW1.3/Navigation/Navigation/PostViewController.swift
+++ HW1.3/Navigation/Navigation/PostViewController.swift
@@ -7,12 +7,13 @@
 
 import UIKit
 
+//шестое задание
+
 class PostViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .systemBlue
     }
     
 
--- HW1.3/Navigation/Navigation/TabBarController.swift
+++ HW1.3/Navigation/Navigation/TabBarController.swift
@@ -8,11 +8,34 @@
 import UIKit
 
 class TabBarController: UITabBarController {
+    
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            let FirstViewController = FeedViewController()  // пятое задание
+            let firstNavigationController = UINavigationController(rootViewController: FirstViewController)
+            
+            let SecondViewController = ProfileViewController()   // пятое задание
+            let secondNavigationController = UINavigationController(rootViewController: SecondViewController)
+            
+            viewControllers = [firstNavigationController, secondNavigationController]
+            
+            // Четвертый пункт
+            self.tabBar.tintColor = UIColor.black
+            self.tabBar.isTranslucent = false
+            
+            self.view.backgroundColor = .white
+            
+            let appearance = UITabBarItem.appearance()
+            let attributes = [NSAttributedString.Key.font:UIFont(name: "TimesNewRomanPSMT", size: 15)]
+            appearance.setTitleTextAttributes(attributes, for: .normal)
+            
+            firstNavigationController.tabBarItem.image = UIImage(systemName: "n.circle")
+            firstNavigationController.tabBarItem.title = "Лента новостей"
+            
+            secondNavigationController.tabBarItem.image = UIImage(systemName: "list.dash")
+            secondNavigationController.tabBarItem.title = "Профиль пользователя"
+        }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
 
@@ -26,4 +49,3 @@ class TabBarController: UITabBarController {
     }
     */
 
-}

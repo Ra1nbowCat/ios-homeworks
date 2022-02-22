2821d5fedc7dfba2acd5905fa44205f8f057a31b
Lekhov Ilya <solwanlomtohs@mail.ru>
2022-02-22 15:16:43 +0300

    Dorabotka 1

--- HW1.2/Netology_IB_Instrument/ProfileView.swift
+++ HW1.2/Netology_IB_Instrument/ProfileView.swift
@@ -19,5 +19,26 @@ class ProfileView: UIView {
     
     @IBOutlet var descriptionText: UITextView!
     
-    
+    override init(frame: CGRect) {
+            super.init(frame: frame)
+            self.setupView()
+        }
+        
+        required init?(coder: NSCoder) {
+            super.init(coder: coder)
+            self.setupView()
+        }
+        
+        private func setupView() {
+            let view = self.loadViewFromXib()
+            view.frame = self.bounds
+            view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            self.addSubview(view)
+        }
+        
+        private func loadViewFromXib() -> UIView {
+            guard let view = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? UIView else { return UIView() }
+            
+            return view
+        }
 }

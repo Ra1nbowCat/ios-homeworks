5949728ae4209c318096b363da7a0c79a41f75f6
Lekhov Ilya <solwanlomtohs@mail.ru>
2022-03-19 12:25:21 +0300

    Created color and Log In class

---
+++ HW1.3/Navigation/Navigation/Профиль/LogInViewController.swift
@@ -0,0 +1,51 @@
+//
+//  LogInViewController.swift
+//  Navigation
+//
+//  Created by Илья Лехов on 19.03.2022.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+    
+    var newColor = UIColor(rgb: 0x4885CC)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
+extension UIColor {
+   convenience init(red: Int, green: Int, blue: Int) {
+       assert(red >= 0 && red <= 255, "Invalid red component")
+       assert(green >= 0 && green <= 255, "Invalid green component")
+       assert(blue >= 0 && blue <= 255, "Invalid blue component")
+
+       self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+   }
+
+   convenience init(rgb: Int) {
+       self.init(
+           red: (rgb >> 16) & 0xFF,
+           green: (rgb >> 8) & 0xFF,
+           blue: rgb & 0xFF
+       )
+   }
+}

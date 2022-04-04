c9ff4257b61930e023b3bd1255e8f65d384f496b
Lekhov Ilya <solwanlomtohs@mail.ru>
2022-04-04 10:40:23 +0300

    Added status validation

--- HW1.3/Navigation/Navigation/Профиль/ProfileHeaderView.swift
+++ HW1.3/Navigation/Navigation/Профиль/ProfileHeaderView.swift
@@ -9,82 +9,7 @@ import UIKit
 
 class ProfileHeaderView: UIView {
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupViews()
-        addConstraintsToView()
-        
-        let toolBar = UIToolbar(frame: CGRect(x: 0, y: 0, width: self.frame.size.width, height: 50))
-        let flexibleSpase = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
-        let doneButton = UIBarButtonItem(title: "Done", style: .done , target: self, action: #selector(didTapDone))
-        toolBar.items = [flexibleSpase, doneButton]
-        toolBar.sizeToFit()
-        statusTextField.inputAccessoryView = toolBar
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("Cannot find coder")
-    }
-    
-    @objc func didTapDone() {
-        statusTextField.resignFirstResponder()
-    }
-    
-    func addConstraintsToView() {
-        NSLayoutConstraint.activate([
-            newImageView.widthAnchor.constraint(equalToConstant: 150),
-            newImageView.heightAnchor.constraint(equalToConstant: 150),
-            newImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
-            newImageView.topAnchor.constraint(equalTo: topAnchor, constant: 16)
-            ])
-        
-        NSLayoutConstraint.activate([
-            mainLabel.widthAnchor.constraint(equalToConstant: 150),
-            mainLabel.heightAnchor.constraint(equalToConstant: 18),
-            mainLabel.topAnchor.constraint(equalTo: topAnchor, constant: 27),
-            mainLabel.leadingAnchor.constraint(equalTo: newImageView.trailingAnchor, constant: 20)
-            ])
-        
-        NSLayoutConstraint.activate([
-            statusLabel.widthAnchor.constraint(equalToConstant: 150),
-            statusLabel.heightAnchor.constraint(equalToConstant: 18),
-            statusLabel.bottomAnchor.constraint(equalTo: newImageView.bottomAnchor, constant: -38),
-            statusLabel.leadingAnchor.constraint(equalTo: newImageView.trailingAnchor, constant: 20)
-            ])
-        
-        NSLayoutConstraint.activate([
-            showStatusButton.heightAnchor.constraint(equalToConstant: 50),
-            showStatusButton.topAnchor.constraint(equalTo: newImageView.bottomAnchor, constant: 16),
-            showStatusButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
-            showStatusButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
-            ])
-        
-        NSLayoutConstraint.activate([
-            statusTextField.heightAnchor.constraint(equalToConstant: 40),
-            statusTextField.leadingAnchor.constraint(equalTo: newImageView.trailingAnchor, constant: 20),
-            statusTextField.bottomAnchor.constraint(equalTo: showStatusButton.topAnchor, constant: -10),
-            statusTextField.trailingAnchor.constraint(equalTo: showStatusButton.trailingAnchor, constant: 0)
-            ])
-    }
-    
-    func setupViews() {
-        self.addSubview(newImageView)
-        self.addSubview(mainLabel)
-        self.addSubview(statusLabel)
-        self.addSubview(showStatusButton)
-        self.addSubview(statusTextField)
-    }
-    
-    @IBAction func buttonPressed(sender: UIButton){
-        statusLabel.text = statusText
-        print(statusLabel.text ?? "No status, sorry :(")
-    }
-    
     private var statusText: String = ""
-    
-    @objc func statusTextChanged(_ textField: UITextField) {
-        statusText = statusTextField.text!
-    }
     
     let contenView: UIView = {
         let view = UIView(frame: CGRect())
@@ -161,4 +86,84 @@ class ProfileHeaderView: UIView {
         textField.translatesAutoresizingMaskIntoConstraints = false
         return textField
     } ()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        addConstraintsToView()
+        
+        let toolBar = UIToolbar(frame: CGRect(x: 0, y: 0, width: self.frame.size.width, height: 50))
+        let flexibleSpase = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        let doneButton = UIBarButtonItem(title: "Done", style: .done , target: self, action: #selector(didTapDone))
+        toolBar.items = [flexibleSpase, doneButton]
+        toolBar.sizeToFit()
+        statusTextField.inputAccessoryView = toolBar
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("Cannot find coder")
+    }
+    
+    func setupViews() {
+        self.addSubview(newImageView)
+        self.addSubview(mainLabel)
+        self.addSubview(statusLabel)
+        self.addSubview(showStatusButton)
+        self.addSubview(statusTextField)
+    }
+    
+    func addConstraintsToView() {
+        NSLayoutConstraint.activate([
+            newImageView.widthAnchor.constraint(equalToConstant: 150),
+            newImageView.heightAnchor.constraint(equalToConstant: 150),
+            newImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            newImageView.topAnchor.constraint(equalTo: topAnchor, constant: 16)
+            ])
+        
+        NSLayoutConstraint.activate([
+            mainLabel.widthAnchor.constraint(equalToConstant: 150),
+            mainLabel.heightAnchor.constraint(equalToConstant: 18),
+            mainLabel.topAnchor.constraint(equalTo: topAnchor, constant: 27),
+            mainLabel.leadingAnchor.constraint(equalTo: newImageView.trailingAnchor, constant: 20)
+            ])
+        
+        NSLayoutConstraint.activate([
+            statusLabel.widthAnchor.constraint(equalToConstant: 150),
+            statusLabel.heightAnchor.constraint(equalToConstant: 18),
+            statusLabel.bottomAnchor.constraint(equalTo: newImageView.bottomAnchor, constant: -68),
+            statusLabel.leadingAnchor.constraint(equalTo: newImageView.trailingAnchor, constant: 20)
+            ])
+        
+        NSLayoutConstraint.activate([
+            showStatusButton.heightAnchor.constraint(equalToConstant: 50),
+            showStatusButton.topAnchor.constraint(equalTo: newImageView.bottomAnchor, constant: 16),
+            showStatusButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            showStatusButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            ])
+        
+        NSLayoutConstraint.activate([
+            statusTextField.heightAnchor.constraint(equalToConstant: 40),
+            statusTextField.leadingAnchor.constraint(equalTo: newImageView.trailingAnchor, constant: 20),
+            statusTextField.bottomAnchor.constraint(equalTo: showStatusButton.topAnchor, constant: -20),
+            statusTextField.trailingAnchor.constraint(equalTo: showStatusButton.trailingAnchor, constant: 0)
+            ])
+    }
+    
+    @objc func didTapDone() {
+        statusTextField.resignFirstResponder()
+    }
+    
+    @IBAction func buttonPressed(sender: UIButton){
+        if statusTextField.text == ""  {
+            statusLabel.textColor = .red
+            statusLabel.text = "Error: empty status"
+        } else {
+            statusLabel.textColor = .darkGray
+            statusLabel.text = statusText
+        }
+    }
+    
+    @objc func statusTextChanged(_ textField: UITextField) {
+        statusText = statusTextField.text!
+    }
 }
